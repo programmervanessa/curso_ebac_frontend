d641bd2959ab275a200f9ba31ca348e2e56aa424
dev-vanessadias <dev.vanessadias@gmail.com>
2023-01-16 12:33:40 -0300

    initial commit

---
+++ ebac-js-jquery-exercicio/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('header button').click(function() {
+        alert("Expandir formulário")
+    })
+
+    $('form').on('submit', function(e) {
+        console.log
+    })
+})
